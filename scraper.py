b20a25c49f91c6d21d638ab206841f953185bd4c
jbwuk <webmaster@jbw-uk.co.uk>
2015-06-30 15:15:20 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,42 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import urllib2
+import lxml.etree
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# get list fo all pdfs to scrape:
+#http://www.london-gazette.co.uk/issues/1960-01-01;2012-11-01/exact=licence+to+abstract+water;sort=oldest/start=1
+#http://www.london-gazette.co.uk/issues/1960-01-01;2012-11-01/exact=licence+to+abstract+water;sort=oldest/start=16752
+
+#for i in range(1,16753):
+record={}
+#n=0
+n=5616
+#12251 #example of both types
+#for i in range(1,16753,10):
+#for i in range(561,16753,10):
+for i in range(5311,16753,10):
+    scraperwiki.sqlite.save_var('last_page', i)
+    url = "http://www.london-gazette.co.uk/issues/1960-01-01;2012-11-01/exact=licence+to+abstract+water;sort=oldest/start=%s" % i
+    #html=scraperwiki.scrape(url)
+    #root = lxml.html.fromstring(html)  
+    root=lxml.html.fromstring(scraperwiki.scrape(url))
+    ax =root.cssselect('li.lteIE6_first-child a')
+    for a in ax:
+        n=n+1
+        if a.attrib['typeof'] in 'g:Notice' : #link to notice exists
+            record={}
+            record['page_id']=i        
+            record['id']=n    
+            record['sourceurl']=url
+            record['link']=a.attrib['about']
+            scraperwiki.sqlite.save(['link'], record, 'websites')
+        else: #only link to pdf
+            record={}
+            record['page_id']=i
+            record['id']=n    
+            record['sourceurl']=url
+            record['pdflink']='http://www.london-gazette.co.uk' + a.attrib['href'] + '/page.pdf'
+            scraperwiki.sqlite.save(['pdflink'], record, 'pdfs')
+        
+    #print scraperwiki.scrape(url)
